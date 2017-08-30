a05940c4757d24ea62c3bd4277e34c170e66945b
Torben Voltmer <git@t-voltmer.net>
2017-08-30 12:39:42 +0200

    Auto stash before merge of "master" and "origin/master"

--- asalu.vhd
+++ asalu.vhd
@@ -19,10 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -68,7 +65,6 @@ begin
 			else
 				equal <= '0';
 				
-			end if;
 		end if;
 	end process;
 
